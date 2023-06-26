2d06bdd487740138f3641bcadc9beba7cfdd4706
Hegi29 <hegitrisaputra@gmail.com>
2023-06-26 11:24:58 +0700

    test: add create contact script

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as router from 'react-router';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import ContactWrapper from './containers/ContactWrapper';
+import FormContact from './containers/FormContact';
 
 const navigate = jest.fn();
 
@@ -18,6 +19,7 @@ beforeEach(() => {
 test('renders contact apps title', () => {
   store = mockStore(initialState);
   render(<Provider store={store}><ContactWrapper /></Provider>);
+
   const linkElement = screen.getByText(/contact apps/i);
   expect(linkElement).toBeInTheDocument();
 });
@@ -25,6 +27,7 @@ test('renders contact apps title', () => {
 test('renders add new button', () => {
   store = mockStore(initialState);
   render(<Provider store={store}><ContactWrapper /></Provider>);
+
   const buttonAdd = screen.getByText(/add new contact/i);
   expect(buttonAdd).toBeInTheDocument();
 });
@@ -32,6 +35,7 @@ test('renders add new button', () => {
 test('renders footer', () => {
   store = mockStore(initialState);
   render(<Provider store={store}><ContactWrapper /></Provider>);
+
   const footerText = screen.getByText(/Hegi Tri Saputra - 2023/i);
   expect(footerText).toBeInTheDocument();
 });
@@ -39,6 +43,58 @@ test('renders footer', () => {
 test('renders table list contact', () => {
   store = mockStore(initialState);
   render(<Provider store={store}><ContactWrapper /></Provider>);
+
   const tableList = screen.getByTestId('table-list');
   expect(tableList).toBeInTheDocument();
+});
+
+test('contact form', () => {
+  store = mockStore(initialState);
+  render(<Provider store={store}><FormContact /></Provider>);
+
+  const fieldFirstName = screen.getByLabelText('First Name');
+  const fieldLastName = screen.getByLabelText('Last Name');
+  const fieldAge = screen.getByLabelText('Age');
+  const fieldPhoto = screen.getByLabelText('URL Photo');
+
+  expect(fieldFirstName).toBeInTheDocument();
+  expect(fieldLastName).toBeInTheDocument();
+  expect(fieldAge).toBeInTheDocument();
+  expect(fieldPhoto).toBeInTheDocument();
+});
+
+test('add new contact', () => {
+  store = mockStore(initialState);
+  render(<Provider store={store}><FormContact /></Provider>);
+
+  const fieldFirstName = screen.getByLabelText('First Name');
+  const fieldLastName = screen.getByLabelText('Last Name');
+  const fieldAge = screen.getByLabelText('Age');
+  const fieldPhoto = screen.getByLabelText('URL Photo');
+  const buttonSave = screen.getByText(/save/i);
+
+  fireEvent.change(fieldFirstName, { target: { value: 'john' } });
+  fireEvent.change(fieldLastName, { target: { value: 'doe' } });
+  fireEvent.change(fieldAge, { target: { value: '20' } });
+  fireEvent.change(fieldPhoto, { target: { value: '-' } });
+
+  expect(fieldFirstName).toHaveDisplayValue("john");
+  expect(fieldLastName).toHaveDisplayValue("doe");
+  expect(fieldAge).toHaveDisplayValue("20");
+  expect(fieldPhoto).toHaveDisplayValue("-");
+
+  fireEvent.click(buttonSave);
+
+  setTimeout(() => {
+    const confirm = screen.getByText(/confirmation/i);
+    expect(confirm).toBeInTheDocument();
+    const buttonYes = screen.getByText(/yes/i);
+    expect(buttonYes).toBeInTheDocument();
+    fireEvent.click(buttonYes);
+
+    setTimeout(() => {
+      const infoSuccess = screen.getByText(/Successully added, this message will disappear in 2 seconds/i);
+      expect(infoSuccess).toBeInTheDocument();
+    }, 2000)
+  }, 2000)
 });
